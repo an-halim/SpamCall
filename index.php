0c388f6839b0ca992d89851ba7b58ffa5b677265
Ahmad Nur Halim <34018853+an-halim@users.noreply.github.com>
2021-01-16 21:02:28 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require'function.php';
 
 
 echo "###########################\n";
-echo  color('blue', "\tSPAM CALL\n\tby halim\nTolong digunakan sewajarnya\n");
+echo  color('blue', "\tSPAM CALL\n\tby halim\ntolong digunakan sewajarnya\n");
 echo "###########################\n";
 echo "[?] No. HP (+62) : ";
 $no = trim(fgets(STDIN));
@@ -14,7 +14,7 @@ echo "[?] Jumlah : ";
 $jml = trim(fgets(STDIN));
 
 if ($jml > 10) {
-	echo "MARUK AMAT ANJING!";
+	echo "MARUK AMAT ANJING!\n";
 }else{
 	for ($i=1; $i <= $jml ; $i++) { 
 
@@ -24,12 +24,11 @@ if ($jml > 10) {
 		if ($spam == true) {
 			echo color('green', "[".$i."/".$jml."] Sukses spam ke +62". $no."\n");
 		}else{
-			echo color('red', "[".$i."/".$jml."] Gagal spam ke +62". $no." *No salah atau LIMIT anjg\n");
-			sleep(10);
+			die(color('red', "[".$i."/".$jml."] Gagal spam ke +62". $no." *No salah atau LIMIT anjg\n"));
 		}
 	}
 
-	echo "SIAP SIAP KENA KARMA YA ANJG!";
+	echo "SIAP SIAP KENA KARMA YA ANJG!\n";
 
 }
 
